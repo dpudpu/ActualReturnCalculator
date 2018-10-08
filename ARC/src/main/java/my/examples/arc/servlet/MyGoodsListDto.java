ec1c6c8923a6459f237efb073ce179a2ae7bd1d9
DaejunBae <DaejunBae>
2018-10-08 18:11:31 +0900

    나의 투자상품 리스트 출력 게시판 1차 (메모리에 데이터 저장 후 출력)

---
+++ ARC/src/main/java/my/examples/arc/servlet/MyGoodsListDto.java
@@ -0,0 +1,10 @@
+package my.examples.arc.servlet;
+
+public class MyInvestList {
+    private String id;
+    private String goodsName;
+    private int investPeriod;
+    private int myPrice;
+    private int profitRatio;
+    private int commissions;
+}
