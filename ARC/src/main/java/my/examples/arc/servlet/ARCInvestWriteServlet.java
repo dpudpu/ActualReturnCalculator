62c5c34dfa81c39310476e37a5e446e8ba72bfb2
Taehee Jang <jang0913@gmail.com>
2018-10-10 09:57:23 +0900

    투자상품 게시판 서블릿 생성

--- ARC/src/main/java/my/examples/arc/servlet/ARCInvestWriteServlet.java
+++ ARC/src/main/java/my/examples/arc/servlet/ARCInvestWriteServlet.java
@@ -1,4 +1,20 @@
 package my.examples.arc.servlet;
 
-public class ARCInvestWriteServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/investment/input")
+public class ARCInvestWriteServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/investinput.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
--- ARC/webapp/WEB-INF/views/inventinput.jsp
+++ ARC/webapp/WEB-INF/views/inventinput.jsp
@@ -6,11 +6,18 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
-    <title>$Title$</title>
+    <title>투자 상품 입력</title>
 </head>
 <body>
-$END$
+    <form method="post" action="/investment/input">
+        <h1>투자 상품 입력</h1>
+        상품 이름 : <input type="text" name="productname"><br>
+        투자 금액(원) : <input type="number" name="mymoney" min="1"><br>
+        투자 기간(개월) : <input type="number" name="investperiod" min="1"><br>
+        <input type="submit" value="submit">
+    </form>
 </body>
 </html>
