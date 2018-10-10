a0dbbcf9b4bff890f0f48bd1453108711ac95d9b
Taehee Jang <jang0913@gmail.com>
2018-10-10 11:01:59 +0900

    투자 입려부분 1차 완성

--- ARC/src/main/java/my/examples/arc/servlet/ARCInvestWriteServlet.java
+++ ARC/src/main/java/my/examples/arc/servlet/ARCInvestWriteServlet.java
@@ -12,9 +12,18 @@ import java.io.IOException;
 public class ARCInvestWriteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+
+        // 아직 상품 목록 불러와서 고르기는 안됨
+        String invPrdName = req.getParameter("productname");
+        int invMoney = Integer.parseInt(req.getParameter("mymoney"));
+        int invPeriod = Integer.parseInt(req.getParameter("investperiod"));
+
+        ARCInvInputDto arcInvInputDto =
+                new ARCInvInputDto(invPrdName, invMoney, invPeriod);
+        ArcDao arcDao = new ArcDao(arcInvInputDto);
+
 
         RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/investinput.jsp");
-        dispatcher.forward(req, resp);
+        resp.sendRedirect("/list");
     }
 }
